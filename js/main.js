004e9e6d0232aec7e6620b341545f2f81f88d26a
Eng.Abdelrhman <79215297+Eldod0101@users.noreply.github.com>
2024-10-28 02:00:00 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -461,7 +461,7 @@ function backToTop() {
 
       db.collection("orders").add(orderData)
         .then(() => {
-          showSendOrderDoneToast();
+         
           document.getElementById("orderForm").reset();
           localStorage.removeItem("cart"); // Clear cart after order
           displayCartItems(); // Refresh cart display
